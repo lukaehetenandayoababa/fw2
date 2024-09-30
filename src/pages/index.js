c395bfe8b289229f2784fc1413daddca4831a390
lukaehetenandayoababa <120053228+lukaehetenandayoababa@users.noreply.github.com>
2024-09-30 15:44:38 +0000

    aula 30/09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
-function Index(){
-    return
-    <>
-<h1>olá mundo</h1>
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Index() {
+    return <>
+        <Container>
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
     </>
 
 
